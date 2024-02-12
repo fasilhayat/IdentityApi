38cf0a518b731a30170022f88ce73d4661f89bef
Fasil Malik Hayat <fasil.hayat@gmail.com>
2024-02-12 19:44:28 +0100

    Updared

--- IdentityApi/Program.cs
+++ IdentityApi/Program.cs
@@ -1,44 +1,38 @@
-namespace IdentityApi;
-
-using Model;
 using System.Collections.Concurrent;
 using System.Text.Json;
+using IdentityApi.Model;
 
-public class Program
+const string filePath = @"var/data/medlemmer.json";
+var json = JsonDocument.Parse(File.ReadAllText(filePath));
+var deserialized = json.Deserialize<IEnumerable<Medlem>>();
+
+var identityStore = new ConcurrentDictionary<uint, string>();
+var cprStore = new ConcurrentDictionary<string, uint>();
+
+if (deserialized != null)
 {
-    public static void Main(string[] args)
+    foreach (var medlem in deserialized)
     {
-        var filePath = @"var/data/medlemmer.json";
-        var json = JsonDocument.Parse(File.ReadAllText(filePath));
-        var deserialized = json.Deserialize<List<Medlem>>();
-
-        var identityStore = new ConcurrentDictionary<uint, string>();
-        var cprStore = new ConcurrentDictionary<string, uint>();
-
-        if (deserialized != null)
-            foreach (var medlem in deserialized)
-            {
-                identityStore.TryAdd(medlem.Identitetsnoegle, medlem.Cprnummer!);
-                cprStore.TryAdd(medlem.Cprnummer!, medlem.Identitetsnoegle);
-            }
-        
-        var builder = WebApplication.CreateBuilder(args);
-        var app = builder.Build();
-
-        app.MapGet("/id/{id}", (uint id) => GetCpr(id, identityStore));
-        app.MapGet("/cpr/{cpr}", (string cpr) =>  GetIdentitetsnoegle(cpr, cprStore));
-        app.Run();
+        identityStore.TryAdd(medlem.Identitetsnoegle, medlem.Cprnummer!);
+        cprStore.TryAdd(medlem.Cprnummer!, medlem.Identitetsnoegle);
     }
+}
 
-    public static IResult GetCpr(uint id, ConcurrentDictionary<uint, string> identityStore)
-    {
-        identityStore.TryGetValue(id, out var cpr);
-        return Results.Content(string.IsNullOrEmpty(cpr) ? "null" : cpr, contentType: "text/plain");
-    }
+var builder = WebApplication.CreateBuilder(args);
+var app = builder.Build();
 
-    public static IResult GetIdentitetsnoegle(string cprnummer, ConcurrentDictionary<string, uint> cprStore)
-    {
-        cprStore.TryGetValue(cprnummer, out var identitetsnoegle);
-        return Results.Content(identitetsnoegle == 0 ? "null" : identitetsnoegle.ToString(), contentType: "text/plain");
-    }
+app.MapGet("/id/{id}", GetCpr);
+app.MapGet("/cpr/{cpr}", GetIdentitetsnoegle);
+app.Run();
+
+IResult GetCpr(uint id)
+{
+    identityStore.TryGetValue(id, out var cpr);
+    return Results.Content(string.IsNullOrEmpty(cpr) ? "null" : cpr, contentType: "text/plain");
+}
+
+IResult GetIdentitetsnoegle(string cprnummer)
+{
+    cprStore.TryGetValue(cprnummer, out var identitetsnoegle);
+    return Results.Content(identitetsnoegle == 0 ? "null" : identitetsnoegle.ToString(), contentType: "text/plain");
 }
