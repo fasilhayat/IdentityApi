725a94d701d2b5231b49b3b612be1caee2ca9a98
Fasil Malik Hayat <fmh@forca.dk>
2024-02-13 15:53:59 +0100

    Updated with status code 404

--- IdentityApi/Program.cs
+++ IdentityApi/Program.cs
@@ -10,13 +10,11 @@ var identityStore = new ConcurrentDictionary<uint, string>();
 var cprStore = new ConcurrentDictionary<string, uint>();
 
 if (deserialized != null)
-{
     foreach (var medlem in deserialized)
     {
         identityStore.TryAdd(medlem.Identitetsnoegle, medlem.Cprnummer!);
         cprStore.TryAdd(medlem.Cprnummer!, medlem.Identitetsnoegle);
     }
-}
 
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
@@ -28,11 +26,11 @@ app.Run();
 IResult GetCpr(uint id)
 {
     identityStore.TryGetValue(id, out var cpr);
-    return Results.Content(string.IsNullOrEmpty(cpr) ? "null" : cpr, contentType: "text/plain");
+    return Results.Content(cpr, contentType: "text/plain", statusCode: string.IsNullOrEmpty(cpr) ? 404 : 200);
 }
 
 IResult GetIdentitetsnoegle(string cprnummer)
 {
     cprStore.TryGetValue(cprnummer, out var identitetsnoegle);
-    return Results.Content(identitetsnoegle == 0 ? "null" : identitetsnoegle.ToString(), contentType: "text/plain");
+    return Results.Content(identitetsnoegle.ToString(), contentType: "text/plain", statusCode: identitetsnoegle == 0 ? 404 : 200);
 }
