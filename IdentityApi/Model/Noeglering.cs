98753156cf40c69110cba587c2e78663fd9d2425
Fasil Malik Hayat <fasil.hayat@gmail.com>
2024-02-14 14:40:07 +0100

    Tilføjet

---
+++ IdentityApi/Model/Noeglering.cs
@@ -0,0 +1,32 @@
+﻿using System.Text.Json.Serialization;
+
+namespace IdentityApi.Model;
+
+/// <summary>
+/// 
+/// </summary>
+public class Noeglering
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="identitetsnoegle"></param>
+    /// <param name="cprnummer"></param>
+    public Noeglering(uint identitetsnoegle, string? cprnummer)
+    {
+        Identitetsnoegle = identitetsnoegle;
+        Cprnummer = cprnummer;
+    }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    [JsonPropertyName("identitetsnoegle")]
+    public uint Identitetsnoegle { get; init; }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    [JsonPropertyName("cprnummer")]
+    public string? Cprnummer { get; init; }
+}
