2a7049fa70c3da1cc53a9588b0335686c1238afc
Fasil Malik Hayat <fasil.hayat@gmail.com>
2024-02-14 09:39:25 +0100

    Frozen dicts

--- IdentityApi/Program.cs
+++ IdentityApi/Program.cs
@@ -1,34 +1,31 @@
+using System.Collections.Frozen;
 using System.Text.Json;
 using IdentityApi.Model;
 
 const string filePath = @"var/data/medlemmer.json";
 var json = JsonDocument.Parse(File.ReadAllText(filePath));
-var deserialized = json.Deserialize<IEnumerable<Medlem>>();
-
-var identityStore = new Dictionary<uint, string>();
-var cprStore = new Dictionary<string, uint>();
-
-if (deserialized != null)
-    foreach (var medlem in deserialized)
-    {
-        identityStore.TryAdd(medlem.Identitetsnoegle, medlem.Cprnummer!);
-        cprStore.TryAdd(medlem.Cprnummer!, medlem.Identitetsnoegle);
-    }
+var deserialized = json.Deserialize<IList<Medlem>>();
 
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/id/{id}", GetCpr);
-app.MapGet("/cpr/{cpr}", GetIdentitetsnoegle);
+if (deserialized is { Count: > 0 })
+{
+    var identityStore = deserialized.Select(x => new KeyValuePair<uint,string>(x.Identitetsnoegle, x.Cprnummer!)).ToFrozenDictionary();
+    var cprStore = deserialized.Select(x => new KeyValuePair<string, uint>(x.Cprnummer!, x.Identitetsnoegle)).ToFrozenDictionary();
+
+    app.MapGet("/id/{id}", (uint id) => GetCpr(id, identityStore));
+    app.MapGet("/cpr/{cpr}", (string cpr) => GetIdentitetsnoegle(cpr, cprStore));
+}
 app.Run();
 
-IResult GetCpr(uint id)
+IResult GetCpr(uint id, FrozenDictionary<uint, string> identityStore)
 {
     identityStore.TryGetValue(id, out var cpr);
     return Results.Content(cpr, contentType: "text/plain", statusCode: string.IsNullOrEmpty(cpr) ? 404 : 200);
 }
 
-IResult GetIdentitetsnoegle(string cpr)
+IResult GetIdentitetsnoegle(string cpr, FrozenDictionary<string, uint> cprStore)
 {
     cprStore.TryGetValue(cpr, out var identitetsnoegle);
     return Results.Content(identitetsnoegle == 0 ? string.Empty : identitetsnoegle.ToString(), contentType: "text/plain", statusCode: identitetsnoegle == 0 ? 404 : 200);
