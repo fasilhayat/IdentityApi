79c10fb0758c528c51f1ce6d103894f3418b938e
Fasil Malik Hayat <fasil.hayat@gmail.com>
2024-02-14 14:28:11 +0100

    More cosmetic corrections.

--- IdentityApi/Program.cs
+++ IdentityApi/Program.cs
@@ -2,8 +2,7 @@ using System.Collections.Frozen;
 using System.Text.Json;
 using IdentityApi.Model;
 
-const string filePath = @"var/data/medlemmer.json";
-var json = JsonDocument.Parse(File.ReadAllText(filePath));
+var json = JsonDocument.Parse(File.ReadAllText(@"var/data/medlemmer.json"));
 var medlemmer = json.Deserialize<IList<Medlem>>();
 
 var builder = WebApplication.CreateBuilder(args);
@@ -13,7 +12,6 @@ if (medlemmer is { Count: > 0 })
 {
     var identityStore = medlemmer.ToFrozenDictionary(x => x.Identitetsnoegle, x=> x.Cprnummer!);
     var cprStore = medlemmer.ToFrozenDictionary(x => x.Cprnummer!, x => x.Identitetsnoegle);
-
     app.MapGet("/id/{id}", (uint id) => GetCpr(id, identityStore));
     app.MapGet("/cpr/{cpr}", (string cpr) => GetIdentitetsnoegle(cpr, cprStore));
 }
