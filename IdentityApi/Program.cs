8e7879adc2182f3fc493c609ebc088a1559e8296
Fasil Malik Hayat <fasil.hayat@gmail.com>
2024-02-18 20:08:30 +0100

    Working solution. Bug double invocation of read datafile on cache expiry. Fix needed.

--- IdentityApi/Program.cs
+++ IdentityApi/Program.cs
@@ -2,32 +2,27 @@ using IdentityApi.Model;
 using System.Collections.Frozen;
 using System.Text;
 using System.Text.Json;
+using System.Runtime.Caching;
 
 const string dataSourceFolder = @"var/data";
 const string dataSourceFilePath = @$"{dataSourceFolder}/medlemmer.json";
 
-FileSystemWatcher watcher;
 FrozenDictionary<string, string> identityStore;
 FrozenDictionary<string, string> cprStore;
+ObjectCache? cache = MemoryCache.Default;
 
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-WatchDataSourceFolder();
 LoadSource();
 
-app.MapGet("/id/{id}",(string id) => GetValue(id, identityStore));
-app.MapGet("/cpr/{cpr}", (string cpr) => GetValue(cpr, cprStore));
+app.MapGet("/id/{id}",(string id) => GetValue("identitystore",id));
+app.MapGet("/cpr/{cpr}", (string cpr) => GetValue("cprstore",cpr));
 app.Run();
 
-void WatchDataSourceFolder()
+void AddToCache(string storageName, FrozenDictionary<string, string> inMemoryStorage)
 {
-    watcher = new FileSystemWatcher();
-    watcher.Path = dataSourceFolder;
-    watcher.NotifyFilter = NotifyFilters.LastWrite;
-    watcher.Filter = "*.json";
-    watcher.Changed += OnChanged;
-    watcher.EnableRaisingEvents = true;
+    cache.Add(storageName, inMemoryStorage, new CacheItemPolicy { SlidingExpiration = new TimeSpan(0, 0, 60), RemovedCallback = CacheRemovedCallback});
 }
 
 void LoadSource()
@@ -35,19 +30,24 @@ void LoadSource()
     Console.WriteLine("Reading json datafile");
     var json = JsonDocument.Parse(File.ReadAllText(dataSourceFilePath, Encoding.UTF8));
     var forretningsnoegler = json.Deserialize<IList<Noeglering>>();
+
     identityStore = forretningsnoegler!.ToFrozenDictionary(x => x.Identitetsnoegle!, x => x.Cprnummer!);
+    AddToCache("identitystore", identityStore);
+
     cprStore = forretningsnoegler!.ToFrozenDictionary(x => x.Cprnummer!, x => x.Identitetsnoegle!);
+    AddToCache("cprstore", cprStore);
     Console.WriteLine($"identitystore size: {identityStore.Count} | cprstore size: {cprStore.Count}");
 }
 
-void OnChanged(object source, FileSystemEventArgs e)
+IResult GetValue(string cacheItemKey, string key)
 {
-    Thread.Sleep(100); // Wait for release lock on file
-    LoadSource();
+    var cachedItem = (IReadOnlyDictionary<string, string>) cache.GetCacheItem(cacheItemKey)!.Value;
+    cachedItem.TryGetValue(key, out var value);
+    return Results.Content(value, contentType: "text/plain", statusCode: string.IsNullOrEmpty(value) ? 404 : 200);
 }
 
-IResult GetValue(string key, IReadOnlyDictionary<string, string> storage)
+void CacheRemovedCallback(CacheEntryRemovedArguments arguments)
 {
-    storage.TryGetValue(key, out var value);
-    return Results.Content(value, contentType: "text/plain", statusCode: string.IsNullOrEmpty(value) ? 404 : 200);
+    Console.WriteLine($"Cached empty: {arguments.RemovedReason} | Item {arguments.CacheItem.Key} removed - reloading data");
+    LoadSource();
 }
