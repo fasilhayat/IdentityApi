201edc7cfa5a43b7f0c552a8188e25e07bf6751a
Malik F. Hayat <fasil.hayat@gmail.com>
2024-04-08 17:48:00 +0200

    Work in progress

--- IdentityApi/Program.cs
+++ IdentityApi/Program.cs
@@ -1,53 +1,51 @@
 using IdentityApi.Model;
-using System.Collections.Frozen;
+using System.Collections.Immutable;
+using System.Runtime.Caching;
 using System.Text;
 using System.Text.Json;
-using System.Runtime.Caching;
 
 const string dataSourceFolder = @"var/data";
 const string dataSourceFilePath = @$"{dataSourceFolder}/medlemmer.json";
-
-FrozenDictionary<string, string> identityStore;
-FrozenDictionary<string, string> cprStore;
-ObjectCache? cache = MemoryCache.Default;
-TimeSpan slidingExpïryTime = new(0, 0, 25);
-
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
 LoadSource();
-app.MapGet("/id/{id}",(string id) => GetValue("identity",id));
-app.MapGet("/cpr/{cpr}", (string cpr) => GetValue("cpr",cpr));
+app.MapGet("/id/{id}", static (string id) => GetValue("identity", id));
+app.MapGet("/cpr/{cpr}", static (string cpr) => GetValue("cpr", cpr));
 app.Run();
 
-void AddToCache(string cacheKey, FrozenDictionary<string, string> inMemoryStorage)
-{
-    cache.Add(cacheKey, inMemoryStorage, new CacheItemPolicy { SlidingExpiration = slidingExpïryTime, RemovedCallback = CacheRemovedCallback});
-}
-
-void LoadSource()
+static void LoadSource()
 {
     Console.WriteLine("Reading json datafile");
     var json = JsonDocument.Parse(File.ReadAllText(dataSourceFilePath, Encoding.UTF8));
     var forretningsnoegler = json.Deserialize<IList<Noeglering>>();
 
-    identityStore = forretningsnoegler!.ToFrozenDictionary(x => x.Identitetsnoegle!, x => x.Cprnummer!);
+    var identityStore = forretningsnoegler!.ToImmutableDictionary(x => x.Identitetsnoegle!, x => x.Cprnummer!);
     AddToCache("identity", identityStore);
 
-    cprStore = forretningsnoegler!.ToFrozenDictionary(x => x.Cprnummer!, x => x.Identitetsnoegle!);
-    AddToCache("cpr", cprStore);
-    Console.WriteLine($"identity key count: {identityStore.Count} | cpr key count: {cprStore.Count}");
+    //var cprStore = forretningsnoegler!.ToImmutableDictionary(x => x.Cprnummer!, x => x.Identitetsnoegle!);
+    //AddToCache("cpr", cprStore);
+    //Console.WriteLine($"identity key count: {identityStore.Count} | cpr key count: {cprStore.Count}");
 }
 
-IResult GetValue(string cacheKey, string key)
+static void AddToCache(string cacheKey, ImmutableDictionary<string, string> inMemoryStorage)
 {
-    var cachedItem = (IReadOnlyDictionary<string, string>) cache.GetCacheItem(cacheKey)!.Value;
-    cachedItem.TryGetValue(key, out var value);
-    return Results.Content(value, contentType: "text/plain", statusCode: string.IsNullOrEmpty(value) ? 404 : 200);
+    Console.WriteLine($"Added in memory-cache: '{cacheKey}' | key count: {inMemoryStorage.Count}");
+    MemoryCache.Default.Set(cacheKey, inMemoryStorage, new CacheItemPolicy { SlidingExpiration = new TimeSpan(0, 0, 0, 30), RemovedCallback = CacheRemovedCallback });
+    Console.WriteLine($"Antal keys: {MemoryCache.Default.GetCount()}");
 }
 
-void CacheRemovedCallback(CacheEntryRemovedArguments arguments)
+static void CacheRemovedCallback(CacheEntryRemovedArguments arguments)
 {
     Console.WriteLine($"Cache empty: {arguments.RemovedReason} | cache: '{arguments.CacheItem.Key}' removed - reloading data");
+    GC.Collect();
     LoadSource();
+}
+
+static IResult GetValue(string cacheKey, string key)
+{
+    Console.WriteLine($"Requested cache-key: '{cacheKey}' | key: {key}");
+    var cachedItem = (IReadOnlyDictionary<string, string>)MemoryCache.Default.GetCacheItem(cacheKey)!.Value;
+    cachedItem.TryGetValue(key, out var value);
+    return Results.Content(value, contentType: "text/plain", statusCode: string.IsNullOrEmpty(value) ? 404 : 200);
 }
