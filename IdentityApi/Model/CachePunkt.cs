0c101c1b0875e1863be9c29d15a38cb5a2dcbdb4
Fasil Malik Hayat <fmh@forca.dk>
2024-04-10 08:29:06 +0200

    Running example (local) need docker tests

---
+++ IdentityApi/Model/CachePunkt.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Immutable;
+
+namespace IdentityApi.Model;
+
+public class CachePunkt
+{
+    public CachePunkt(ImmutableDictionary<string, string> cprStore, ImmutableDictionary<string, string> identityStore)
+    {
+        CprStore = cprStore;
+        IdentityStore = identityStore;
+    }
+
+    public ImmutableDictionary<string, string> CprStore { get; }
+
+    public ImmutableDictionary<string, string> IdentityStore { get; }
+}
--- IdentityApi/Program.cs
+++ IdentityApi/Program.cs
@@ -1,53 +1,43 @@
 using IdentityApi.Model;
-using System.Collections.Frozen;
+using System.Collections.Immutable;
+using System.Runtime.Caching;
 using System.Text;
 using System.Text.Json;
-using System.Runtime.Caching;
 
 const string dataSourceFolder = @"var/data";
 const string dataSourceFilePath = @$"{dataSourceFolder}/medlemmer.json";
-
-FrozenDictionary<string, string> identityStore;
-FrozenDictionary<string, string> cprStore;
-ObjectCache? cache = MemoryCache.Default;
-TimeSpan slidingExpïryTime = new(0, 0, 25);
-
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-LoadSource();
-app.MapGet("/id/{id}",(string id) => GetValue("identity",id));
-app.MapGet("/cpr/{cpr}", (string cpr) => GetValue("cpr",cpr));
+LoadDataSource();
+app.MapGet("/id/{id}", static (string id) => GetValue("identity", id));
+app.MapGet("/cpr/{cpr}", static (string cpr) => GetValue("cpr", cpr));
 app.Run();
 
-void AddToCache(string cacheKey, FrozenDictionary<string, string> inMemoryStorage)
+static void LoadDataSource()
 {
-    cache.Add(cacheKey, inMemoryStorage, new CacheItemPolicy { SlidingExpiration = slidingExpïryTime, RemovedCallback = CacheRemovedCallback});
-}
-
-void LoadSource()
-{
-    Console.WriteLine("Reading json datafile");
+    Console.WriteLine($"{DateTime.Now} - Reading json datafile");
     var json = JsonDocument.Parse(File.ReadAllText(dataSourceFilePath, Encoding.UTF8));
     var forretningsnoegler = json.Deserialize<IList<Noeglering>>();
 
-    identityStore = forretningsnoegler!.ToFrozenDictionary(x => x.Identitetsnoegle!, x => x.Cprnummer!);
-    AddToCache("identity", identityStore);
+    var identityStore = forretningsnoegler!.ToImmutableDictionary(x => x.Identitetsnoegle!, x => x.Cprnummer!);
+    var cprStore = forretningsnoegler!.ToImmutableDictionary(x => x.Cprnummer!, x => x.Identitetsnoegle!);
+    var cachePunkt = new CachePunkt(cprStore, identityStore);
 
-    cprStore = forretningsnoegler!.ToFrozenDictionary(x => x.Cprnummer!, x => x.Identitetsnoegle!);
-    AddToCache("cpr", cprStore);
-    Console.WriteLine($"identity key count: {identityStore.Count} | cpr key count: {cprStore.Count}");
+    MemoryCache.Default.Set("IdentityCache", cachePunkt, new CacheItemPolicy { SlidingExpiration = new TimeSpan(0, 0, 2, 0), RemovedCallback = CacheRemovedCallback });
 }
 
-IResult GetValue(string cacheKey, string key)
+static void CacheRemovedCallback(CacheEntryRemovedArguments arguments)
 {
-    var cachedItem = (IReadOnlyDictionary<string, string>) cache.GetCacheItem(cacheKey)!.Value;
-    cachedItem.TryGetValue(key, out var value);
+    LoadDataSource();
+    GC.Collect();
+}
+
+static IResult GetValue(string cacheSubject, string key)
+{
+    var cachedItem = (CachePunkt) MemoryCache.Default.GetCacheItem("IdentityCache")!.Value;
+    var datastore = cacheSubject == "identity" ? cachedItem.IdentityStore : cachedItem.CprStore;
+
+    datastore.TryGetValue(key, out var value);
     return Results.Content(value, contentType: "text/plain", statusCode: string.IsNullOrEmpty(value) ? 404 : 200);
-}
-
-void CacheRemovedCallback(CacheEntryRemovedArguments arguments)
-{
-    Console.WriteLine($"Cache empty: {arguments.RemovedReason} | cache: '{arguments.CacheItem.Key}' removed - reloading data");
-    LoadSource();
 }
